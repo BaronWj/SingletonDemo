7028418151e8db22896f68c85b8133ecea1de456
panweijie <panweijie@okjiaoyu.cn>
2016-07-13 10:36:27 +0800

    Initial Commit

---
+++ SingletonDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingletonDemo
+//
+//  Created by 潘伟杰 on 16/7/13.
+//  Copyright © 2016年 潘伟杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SingletonDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SingletonDemo
+//
+//  Created by 潘伟杰 on 16/7/13.
+//  Copyright © 2016年 潘伟杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
